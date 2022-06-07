639f4cd02e265748686a73aef886468b876a4082
pmecchia <mecchia.pierre@gmail.com>
2022-06-07 10:58:05 +0200

    group functions + add min shot range

--- shot_detection.py
+++ shot_detection.py
@@ -29,9 +29,24 @@ def color_hist(file_path):
             cap.release()
     return list_diff,fps
 
-def define_shots(nb_shots,hist_array,fps):
-    n = len(hist_array)
-    ranked = np.argsort(hist_array)
+
+def visualization(hist_diff,shots):
+    df=pd.DataFrame()
+    x=np.arange(1,len(hist_diff))
+    df["x"]=x
+    df["hist"]=hist_diff[1:]
+    df["color"]=["shot detection" if i in shots else "frames" for i in x]
+
+    fig = px.bar(df,x="x", y="hist", color="color")
+    fig.update_traces(dict(marker_line_width=0))
+    fig.show()
+
+def define_shots(file_path,nb_shots,range_min=1,show_viz=False):
+
+    hist_diff,fps=color_hist(file_path)
+
+    n = len(hist_diff)
+    ranked = np.argsort(hist_diff)
     largest_indices = ranked[::-1][1:n]
 
     shots_index=[0,max(largest_indices)]
@@ -39,7 +54,7 @@ def define_shots(nb_shots,hist_array,fps):
 
         valid=False
         for j in shots_index:
-            if np.abs(j-val)>fps:
+            if np.abs(j-val)>range_min*fps:
                 valid=True
             else:
                 valid=False
@@ -48,31 +63,22 @@ def define_shots(nb_shots,hist_array,fps):
             shots_index.append(val)
 
     shots=np.sort(shots_index[:nb_shots+1])
+
+    if show_viz==True:
+        visualization(hist_diff,shots)
+
     return shots
 
 
-def visualization(hists,shots):
-    df=pd.DataFrame()
-    x=np.arange(1,len(hists))
-    df["x"]=x
-    df["hist"]=hists[1:]
-    df["color"]=["shot detection" if i in shots else "frames" for i in x]
 
-    fig = px.bar(df,x="x", y="hist", color="color")
-    fig.update_traces(dict(marker_line_width=0))
-    fig.show()
 
 
 if __name__ == '__main__':
     file_path=sys.argv[1]
     nb_shots=int(sys.argv[2])
+    range_min=int(sys.argv[3])
+    show_viz=bool(sys.argv[4])
 
-    show_viz=sys.argv[3]
-
-    hist_diff,fps=color_hist(file_path)
-
-    shots=define_shots(nb_shots,hist_diff,fps)
-
+    shots=define_shots(file_path,nb_shots,range_min,show_viz)
     print(shots)
-    if show_viz=='True':
-        visualization(hist_diff,shots)
+
