53f99a29b7b2d7841318f8b4a5a215ed82c87b31
pmecchia <mecchia.pierre@gmail.com>
2022-06-18 12:47:54 +0200

    create summary_frames_selection function

--- generate_summary.py
+++ generate_summary.py
@@ -7,7 +7,73 @@ from tqdm import tqdm
 from shot_detection import ordering_videos
 
 
-def frames_selection(list_frame_index,files_list):
+def summary_frames_selection(summary_duration,summary_fps,shot_percentage,dict_shots_order):
+
+    """
+    Function that generate an array of frame index selected for the summary and output the time in seconds before the most
+    important frame in the summary.
+
+    Input:
+           - summary_duration   : int -> output time in seconds
+           - summary_fps        : int -> number of frames per seconds for summary video in output
+           - shot_percentage   : int -> percentage of the shot kept for the summary
+           - dict_shots_order   : dict -> dictionnary of shots ordering by their importance.
+                                        key: shot number / value: (shot mean, index of max value on the shot, (index first frame,index last frame))
+
+    Output:
+           - summary_frames_index   : array of frame index selected for the summary
+           - time_before_drop       : float -> time in seconds before the most important frame of the summary
+    """
+
+    summary_frames_nb=0                         # counter of number of frames selected
+    output_frames_nb=summary_duration*summary_fps #convert output duration in seconds into frame number
+    summary_frames_index=[]
+
+    best_shot=True
+    for key, value in dict_shots_order.items():
+        first_frame_index=value[2][0]   #first frame index of the current shot
+        last_frame_index=value[2][1]    #last frame index of the current shot
+        if best_shot==True:             #save best frame index of top1 shot
+            best_frame=value[1]
+        shot_frames_nb=last_frame_index-first_frame_index               #number of frames in shots
+        summary_shot_frames_nb=int(shot_frames_nb*shot_percentage/100) #number of frames to select for the summary
+
+        if summary_shot_frames_nb+summary_frames_nb>output_frames_nb: #if length shot exceeds total frames number
+            summary_shot_frames_nb=output_frames_nb-summary_frames_nb #complete by the number of frames missing
+
+
+        slice_inf=value[1]-int(summary_shot_frames_nb/2)                        #lower bound index of selected frames
+        slice_sup=value[1]-int(summary_shot_frames_nb/2)+summary_shot_frames_nb #upper bound index of selected frames
+
+        #conditions for not selecting frames from another shot
+        #the number of frames outside the shot range is compensated on the other side of the bound
+
+        if slice_inf < first_frame_index:        #if lower bound index is out of shot range
+            offset=first_frame_index-slice_inf
+            slice_inf=first_frame_index
+            slice_sup+=offset
+        elif slice_sup > last_frame_index:       #if upper bound index is out of shot range
+            offset=slice_sup-last_frame_index
+            slice_sup=last_frame_index
+            slice_inf-=offset
+
+        shot_selection=list(range(slice_inf,slice_sup)) # generate a list of index between the two bounds
+        summary_frames_nb+=len(shot_selection)          #update number of selected frames
+        summary_frames_index.extend(shot_selection)     #update list of frames selected
+
+        if summary_frames_nb==output_frames_nb:         #break when we reach expected output video length
+            break
+
+    summary_frames_index.sort()                                 #sort list
+    nb_frames_before_top=summary_frames_index.index(best_frame) #index of best frame in list
+
+    time_before_drop=nb_frames_before_top/summary_fps            #time in seconds before the most important frame
+
+
+    return summary_frames_index,time_before_drop
+
+
+def frames_selection_download(list_frame_index,files_list):
 
     """
     Function that keep frames passed in a list of index
@@ -39,7 +105,6 @@ def frames_selection(list_frame_index,files_list):
         pbar.update()
     pbar.close()
     selection = np.array(selection)
-    print(selection.shape)
     return selection
 
 def create_summary(frames_selected,output_filename,width,height,fps):
@@ -74,10 +139,13 @@ def create_summary(frames_selected,output_filename,width,height,fps):
 if __name__ == '__main__':
     dir_path=sys.argv[1]
     result_name="test_result"
-    fps=29 #take min value in dict_video_endIndex_fps from  compute_videos_hist in shot detection file
-    width=1920
+    summary_fps=29 #take min value in dict_video_endIndex_fps from  compute_videos_hist in shot detection file
+    width=1920 #take the min resolution
     height=1080
+    summary_duration=30
+    shot_percentage=50
+    #dict_shots_order -> result from shot_order.py
     files_list= ordering_videos(dir_path)
-    list_frame_index=[23,24,50,53,54,57,59,60,61,62,63,400,402,403,406,700,702,703,704,1500,1501,1502,1503,1504,1505,2500,2501,2502,3400,3401]
-    selection=frames_selection(list_frame_index,files_list)
-    create_summary(selection,result_name,width,height,fps)
+    list_frame_index,time_before_drop=summary_frames_selection(summary_duration,summary_fps,shot_percentage,dict_shots_order)
+    selection=frames_selection_download(list_frame_index,files_list)
+    create_summary(selection,result_name,width,height,summary_fps)
