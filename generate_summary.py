8f3de87a55afe8c32d45710cb2c5ce393df3a365
pmecchia <mecchia.pierre@gmail.com>
2022-06-20 16:46:19 +0200

    create main.py & lighten the code by removing the repetition of the opening and reading of the videos

--- generate_summary.py
+++ generate_summary.py
@@ -1,158 +1,113 @@
-import numpy as np
 import cv2
-import os
-import sys
 import time
-from tqdm import tqdm
-from shot_detection import ordering_videos
+import itertools
 
 
-def summary_frames_selection(summary_duration,summary_fps,shot_percentage,dict_shots_order,min_shot_nb):
-
+def summary_frames_selection(summary_duration, summary_fps, shot_percentage, dict_shots_order, min_shot_nb):
     """
-    Function that generate an array of frame index selected for the summary and output the time in seconds before the most
-    important frame in the summary.
+    Function that generate an array of frame index selected for the summary and output the time in seconds before the
+    most important frame in the summary.
 
     Input:
            - summary_duration   : int -> output time in seconds
            - summary_fps        : int -> number of frames per seconds for summary video in output
            - shot_percentage    : int -> percentage of the shot kept for the summary
-           - dict_shots_order   : dict -> dictionnary of shots ordering by their importance.
-                                        key: shot number / value: (shot mean, index of max value on the shot, (index first frame,index last frame))
+           - dict_shots_order   : dict -> dictionary of shots ordering by their importance.
+                                key: shot number
+                                value: (shot mean, index of max value on the shot, (index first frame,index last frame))
            - min_shot_nb        : int -> minimum of shots that must be included in the summary
     Output:
            - summary_frames_index   : array of frame index selected for the summary
            - time_before_drop       : float -> time in seconds before the most important frame of the summary
     """
 
-    output_frames_nb=summary_duration*summary_fps #convert output duration in seconds into frame number
-    nb_shots_used=0                               # count number of shots included in the summary
+    output_frames_nb = summary_duration * summary_fps  # convert output duration in seconds into frame number
+    nb_shots_used = 0  # count number of shots included in the summary
 
-    while (nb_shots_used<min_shot_nb): # change percentage until the minimum of shots included in the summary is reached
-        nb_shots_used=0                     # reset counter
-        summary_frames_nb=0                 # number of frames selected counter
-        summary_frames_index=[]             # array of frame index selected
+    while nb_shots_used < min_shot_nb:  # change percentage until the min of shots included in the summary is reached
+        nb_shots_used = 0  # reset counter
+        summary_frames_nb = 0  # number of frames selected counter
+        summary_frames_index = []  # array of frame index selected
 
-        best_shot=True
+        best_shot = True
 
         for key, value in dict_shots_order.items():
-            first_frame_index=value[2][0]   # first frame index of the current shot
-            last_frame_index=value[2][1]    # last frame index of the current shot
-            if best_shot==True:             # save best frame index of top1 shot
+            first_frame_index = value[2][0]  # first frame index of the current shot
+            last_frame_index = value[2][1]  # last frame index of the current shot
+            if best_shot:  # save best frame index of top1 shot
                 best_frame = value[1]
-            shot_frames_nb = last_frame_index-first_frame_index               # number of frames in shots
-            summary_shot_frames_nb=int(shot_frames_nb*shot_percentage/100)  # number of frames to select for the summary
+            shot_frames_nb = last_frame_index - first_frame_index  # number of frames in shots
+            summary_shot_frames_nb = int(
+                shot_frames_nb * shot_percentage / 100)  # number of frames to select for the summary
 
-            if summary_shot_frames_nb+summary_frames_nb>output_frames_nb: # if length shot exceeds total frames number
-                summary_shot_frames_nb=output_frames_nb-summary_frames_nb # complete by the number of frames missing
+            if summary_shot_frames_nb + summary_frames_nb > output_frames_nb:  # if length shot exceeds total frames
+                # number
+                summary_shot_frames_nb = output_frames_nb - summary_frames_nb  # complete by the number of frames
+                # missing
 
-
-            slice_inf = value[1]-int(summary_shot_frames_nb/2)                        # lower bound index of selected frames
-            slice_sup = value[1]-int(summary_shot_frames_nb/2)+summary_shot_frames_nb # upper bound index of selected frames
+            slice_inf = value[1] - int(summary_shot_frames_nb / 2)  # lower bound index of selected frames
+            slice_sup = value[1] - int(
+                summary_shot_frames_nb / 2) + summary_shot_frames_nb  # upper bound index of selected frames
 
             # conditions for not selecting frames from another shot
             # the number of frames outside the shot range is compensated on the other side of the bound
 
-            if slice_inf < first_frame_index:        # if lower bound index is out of shot range
-                offset=first_frame_index-slice_inf
-                slice_inf=first_frame_index
-                slice_sup+=offset
-            elif slice_sup > last_frame_index:       # if upper bound index is out of shot range
-                offset=slice_sup-last_frame_index
-                slice_sup=last_frame_index
-                slice_inf-=offset
+            if slice_inf < first_frame_index:  # if lower bound index is out of shot range
+                offset = first_frame_index - slice_inf
+                slice_inf = first_frame_index
+                slice_sup += offset
+            elif slice_sup > last_frame_index:  # if upper bound index is out of shot range
+                offset = slice_sup - last_frame_index
+                slice_sup = last_frame_index
+                slice_inf -= offset
 
-            shot_selection=list(range(slice_inf,slice_sup)) # generate a list of index between the two bounds
-            summary_frames_nb+=len(shot_selection)          # update number of selected frames
-            summary_frames_index.extend(shot_selection)     # update list of frames selected
+            shot_selection = list(range(slice_inf, slice_sup))  # generate a list of index between the two bounds
+            summary_frames_nb += len(shot_selection)  # update number of selected frames
+            summary_frames_index.extend(shot_selection)  # update list of frames selected
 
-            nb_shots_used+=1                                # increase shots number visited
+            nb_shots_used += 1  # increase shots number visited
 
-            if (output_frames_nb-summary_frames_nb < summary_fps):  # break when we reach expected output video length or < one fps unit
+            if output_frames_nb - summary_frames_nb < summary_fps:  # break when we reach expected output video
+                # length or < one fps unit
                 break
 
-        shot_percentage-=5                                  # update percentage -> decrease by 5%
+        shot_percentage -= 5  # update percentage -> decrease by 5%
 
-    summary_frames_index.sort()                                 #sort list
-    nb_frames_before_top=summary_frames_index.index(best_frame) #index of best frame in list
+    summary_frames_index.sort()  # sort list
+    nb_frames_before_top = summary_frames_index.index(best_frame)  # index of best frame in list
 
-    time_before_drop=nb_frames_before_top/summary_fps            #time in seconds before the most important frame
+    time_before_drop = nb_frames_before_top / summary_fps  # time in seconds before the most important frame
 
-    return summary_frames_index,time_before_drop
-
-def frames_selection_download(list_frame_index,files_list):
-
-    """
-    Function that keep frames passed in a list of index
-
-    Input:
-           - list_frame_index: frame index array
-    Output:
-           - selection: array of frames selected
-    """
-                                               #list of files in datetime order
-    pbar = tqdm(desc='Video',total=len(files_list),position=0, leave=True)           #create loading bar
-
-    selection=[]
-    count = 0
-    for file_path in files_list:              #loop over all videos in the directory
-        filename=file_path.split("/")[-1]
-        pbar.set_description(f'Videos -> {filename}')
-        pbar.refresh() # to show immediately the update
-        print(file_path)
-        vidcap = cv2.VideoCapture(file_path)
-        success,image = vidcap.read()
+    return summary_frames_index, time_before_drop
 
 
-        while success:
-            if count in list_frame_index:
-                selection.append(np.asarray(image))
-            success,image = vidcap.read()
-            count += 1
-        pbar.update()
-    pbar.close()
-    selection = np.array(selection)
-    return selection
-
-def create_summary(frames_selected,output_filename,width,height,fps):
-
+def create_summary(frames_list, summary_frames_index, summary_filename, summary_resolution, summary_fps):
     """
     Function that create a mp4 video from frame array
 
     Input:
-           - frames_selected: array of frames selected
-           - output_filename: string -> output file name
-           - width: -> width size
-           - height: int -> height size
-           - fps: int -> frames per seconde
+            - frames_list           : array of arrays -> frames matrix for each video
+            - summary_frames_index  : array of frames index selected
+            - summary_filename      : string -> output file name
+            - summary_resolution    : tuple -> resolution of summary video (width, height)
+            - summary_fps           : int -> frames per seconds for summary video
 
     Output:
            return a mp4 video
     """
 
+    frames_list = list(itertools.chain(*frames_list))  # flatten list
 
-    out = cv2.VideoWriter(output_filename+'.mp4',cv2.VideoWriter_fourcc('M','J','P','G'), fps, (width,height))
-    i=0
-    for frame in frames_selected:
-        out.write(frame)
-        time.sleep(0.7)
-        i+=1
+    out = cv2.VideoWriter(summary_filename + '.mp4', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), summary_fps,
+                          summary_resolution)
+
+    for index, frame in enumerate(frames_list):  # loop over frame list & write frame if index in list selection
+        if index in summary_frames_index:
+            frame_resized = cv2.resize(frame, summary_resolution, interpolation=cv2.INTER_AREA)  # reshape frames on
+            # the same resolution
+            out.write(frame_resized)
+            time.sleep(0.7)
     # When everything done, release the video capture and video write objects
     # Cleanup and save video
     cv2.destroyAllWindows()
     out.release()
-
-
-if __name__ == '__main__':
-    dir_path=sys.argv[1]
-    result_name="test_result"
-    summary_fps=29 #take min value in dict_video_endIndex_fps from  compute_videos_hist in shot detection file
-    width=1920 #take the min resolution
-    height=1080
-    summary_duration=30
-    shot_percentage=50
-    #dict_shots_order -> result from shot_order.py
-    files_list= ordering_videos(dir_path)
-    list_frame_index,time_before_drop=summary_frames_selection(summary_duration,summary_fps,shot_percentage,dict_shots_order)
-    selection=frames_selection_download(list_frame_index,files_list)
-    create_summary(selection,result_name,width,height,summary_fps)
--- shot_detection.py
+++ shot_detection.py
@@ -1,213 +1,107 @@
 import cv2
 import numpy as np
-import sys
 import pandas as pd
 import plotly.express as px
-import os
-from pathlib import Path
 from tqdm import tqdm
-import platform
+import itertools
 
 
-
-
-# System Arguments
-
-# Argument 1: the path to the videos directory
-# Argument 2: Nb slots (int)
-# Argument 3: Show visualization (True/False)
-
-def color_hist(file_path,previous_hist):
-
-    """
-    Function to calculate the color difference histogram between two consecutive frames
-
-    Input:
-           - file path: path of the video
-           - previous_hist: colour histogram of the last frame of the previous video
-    Output:
-           - list_diff: array of color difference histogram
-           - fps : frame rate of the video
-           - last_frame_hist: colour histogram of the last frame of the current video
-    """
-
-
-    cap = cv2.VideoCapture(file_path)
-    fps=int(cap.get(cv2.CAP_PROP_FPS)) #get fps of the video
-
-    list_diff=[]
-
-    while(cap.isOpened()):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret==True:
-            hist = cv2.calcHist([frame], [0, 1, 2],None, [8, 8, 8], [0, 256, 0, 256, 0, 256]) #color histogram
-            frame_diff=np.sum(np.abs(hist-previous_hist)) # hist diff on all channels
-            list_diff.append(frame_diff)
-
-            previous_hist=hist #update histogram of previous frame
-        else:
-            cap.release()
-    last_frame_hist=hist #save color histogram of last frame in order to be use as first frame of next video
-    return list_diff,fps,last_frame_hist
-
-
-def ordering_videos(dir_path):
-    """
-    Function to order videos by last modification date (in epoch time format).
-    useful to keep the videos consistent.
-
-    Input:
-            - dir_path: path of the videos directory
-    Output:
-            - videos_order: array of video's filename ordering by last modification date
-    """
-    videos_order={}                                             #dict -> key:filename / value: Datetime in epoch Time
-    for file in os.listdir(dir_path):
-        if not file.startswith('.'):                                                        # avoid hidden files
-            if platform.system() == 'Windows':
-                video_dateTime= os.path.getctime(dir_path+'/'+file)
-            else:
-                stat = os.stat(dir_path+'/'+file)
-                try:
-                    video_dateTime= stat.st_birthtime
-                except AttributeError:
-                    # We're probably on Linux. No easy way to get creation dates here,
-                    # so we'll settle for when its content was last modified.
-                    video_dateTime=stat.st_mtime
-            videos_order[file]=video_dateTime
-    videos_order=list(dict(sorted(videos_order.items(), key=lambda item: item[1])).keys())  # list of filename
-                                                                                            # ordering dict by dateTime
-    return videos_order
-
-
-def compute_videos_hist(dir_path):
-
+def compute_videos_hist(frames_list):
     """
     Function to calculate the color difference histogram for all the videos in the directory
 
      Input:
-           dir_path: path of the videos directory
+           frames_list      : array of arrays -> frames matrix for each video
      Output:
-           total_list_hist: array of color difference histogram for all videos
-           dict_video_endIndex_fps: dictionary -> key:video last frame index / value: fps
+           list_hist_diff   : array of color difference histogram for all videos
     """
 
+    list_hist_diff = []  # color histogram difference for all the videos
+    previous_hist = np.zeros((8, 8, 8))  # init for first frame diff calculation
 
-    total_list_hist=[]                  #color histogram difference for all the videos
-    dict_video_endIndex_fps={}          #dict key:video last frame index / value: fps
-    last_frame_index=0                  #init for last frame video index
-    previous_hist=np.zeros((8,8,8))     #init for first frame diff calculation
+    pbar = tqdm(desc='Color Histogram computation', total=len(frames_list), position=0,
+                leave=True)  # create loading bar
 
-    #files=[name for name in os.listdir(dir_path) if not name.startswith('.')]   #list video files in directory
-    files= ordering_videos(dir_path)                                            #list of files in datetime order
-    pbar = tqdm(desc='Video',total=len(files),position=0, leave=True)           #create loading bar
+    for frame in frames_list:  # loop over all videos in the directory
+        pbar.refresh()  # to show immediately the update
+        hist = cv2.calcHist([frame], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])  # color histogram
+        frame_diff = np.sum(np.abs(hist - previous_hist))  # hist diff on all channels
+        list_hist_diff.append(frame_diff)
 
-    for filename in files:              #loop over all videos in the directory
-        pbar.set_description(f'Videos -> {filename}')
-        pbar.refresh() # to show immediately the update
-        file_path = os.path.join(dir_path, filename)
-        list_hist,fps,last_frame_hist=color_hist(file_path,previous_hist)   #compute color histogram difference
-        total_list_hist.extend(list_hist)
-        last_frame_index+=len(list_hist)                #last frame index of the video
-        dict_video_endIndex_fps[last_frame_index]=fps   #save for each video the last frame index and its fps
-        previous_hist=last_frame_hist                   #update previous_hist for next video
+        previous_hist = hist  # update histogram of previous frame
+
         pbar.update()
     pbar.close()
-    return total_list_hist,dict_video_endIndex_fps
+    return list_hist_diff
 
-def visualization(total_list_hist,shots):
 
+def visualization(frames_list, shots):
     """
      Function to visualize shots selection depending on color difference histogram
 
      Input:
-           total_list_hist: array of color difference histogram for all videos
-           shots: array of frame index selected as shots separation
+           frames_list  : array of arrays -> frames matrix for each video
+           shots        : array of frame index selected as shots separation
      Output:
            plot figure
     """
 
-
-    df=pd.DataFrame() #init dataframe
-    x=np.arange(1,len(total_list_hist)) #not taking into acount the first and last frame -> shots by default
-    df["x"]=x
-    df["hist"]=total_list_hist[1:]
-    df["color"]=["shot detection" if i in shots else "frames" for i in x] #color legend to identify histograms selected
-                                                                          # as shots
-    fig = px.bar(df,x="x", y="hist", color="color")
+    df = pd.DataFrame()  # init dataframe
+    x = np.arange(1, len(frames_list))  # not taking into account the first and last frame -> shots by default
+    df["x"] = x
+    df["hist"] = frames_list[1:]
+    df["color"] = ["shot detection" if i in shots else "frames" for i in
+                   x]  # color legend to identify histograms selected
+    # as shots
+    fig = px.bar(df, x="x", y="hist", color="color")
     fig.update_traces(dict(marker_line_width=0))
     fig.show()
 
-def define_shots(dir_path,nb_shots,range_min=1,show_viz=False):
 
+def define_shots(frames_list, videos_param, nb_shots, range_min=1, show_viz=False):
     """
      Main function. Divide color difference histogram into a number of shots passed in inout parameter
 
      Input:
-           dir_path: path of the videos directory
-           nb_shots: integer for the number of shots to return
-           range_min: integer -> number of FPS for minimum shot length
-           show_viz: Boolean -> to visualize shots selection depending on color difference histogram
+            frames_list         : array of arrays -> frames matrix for each video
+            videos_param        : dict -> key: file_path / value: [fps,width,height,first_frame_index,last_frame_index]
+            nb_shots            : integer for the number of shots to return
+            range_min           : integer -> number of FPS for minimum shot length
+            show_viz            : Boolean -> to visualize shots selection depending on color difference histogram
      Output:
            shots: array of frame index defined as shots separations
     """
+    frames_list = list(itertools.chain(*frames_list))  # flatten list
 
+    list_hist_diff = compute_videos_hist(
+        frames_list)  # call function to calculate the color difference histogram for each frame
 
-    total_list_hist,dict_video_endIndex_fps=compute_videos_hist(dir_path) #call function to calculate the color
-                                                            # difference histogram for all the videos in the directory
+    n = len(list_hist_diff)  # number of total frames
+    ranked = np.argsort(list_hist_diff)  # sort index by color difference histogram value
+    largest_indices = ranked[::-1][:n]  # invert order
 
-    n = len(total_list_hist)                        #number of total frames
-    ranked = np.argsort(total_list_hist)            #sort index by color difference histogram value
-    largest_indices = ranked[::-1][:n]              #invert order
+    shots_index = [0, n]  # add first and last frame as default shots
+    for i, val in enumerate(largest_indices):  # loop over invert order list
 
-    shots_index=[0,n]                               #add first and last frame as default shots
-    for i,val in enumerate(largest_indices):        #loop over invert order list
-
-        for key in dict_video_endIndex_fps.keys():  #select fps of the corresponding video frame
-            if val<=key:
-                fps=dict_video_endIndex_fps[key]
+        for param in videos_param.values():  # select fps of the frame index
+            if val <= param[4]:  # check index < or equal to first frame index of a video
+                fps = param[0]
                 break
 
-        valid=False                                 #flag: possible to add frame in shot list or not
+        valid = False  # flag: possible to add frame in shot list or not
 
-        for j in shots_index:                       #check if a frame at range_min*fps range is not already a shot
-            if np.abs(j-val)>range_min*fps:
-                valid=True
+        for j in shots_index:  # check if a frame at range_min*fps range is not already a shot
+            if np.abs(j - val) > range_min * fps:
+                valid = True
             else:
-                valid=False
+                valid = False
                 break
-        if valid==True:
+        if valid:
             shots_index.append(val)
 
-    shots=np.sort(shots_index[:nb_shots+1])         #ordering shots array
+    shots = np.sort(shots_index[:nb_shots + 1])  # ordering shots array
 
-
-    if show_viz==True:                              #show visualization
-        visualization(total_list_hist,shots)
+    if show_viz:  # show visualization
+        visualization(list_hist_diff, shots)
 
     return shots
-
-
-if __name__ == '__main__':
-
-    if len(sys.argv) != 4:
-        print ("""
-        Three arguments are required:
-            - The first argument should be the path to the videos directory
-            - The second argument is an integer for the number of shots to create
-            - The third argument is a boolean for showing visualization or not
-            """)
-        sys.exit(0)
-
-    dir_path=sys.argv[1]
-    nb_shots=int(sys.argv[2])
-
-    #range_min=int(sys.argv[3])
-    show_viz=bool(sys.argv[3])
-
-    dir_path = os.path.join(Path(os.getcwd()).absolute(), dir_path)
-
-    shots=define_shots(dir_path,nb_shots,1,show_viz)
-    print(shots)
-
