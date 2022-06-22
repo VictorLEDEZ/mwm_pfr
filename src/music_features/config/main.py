2c89cd22b9de4b1753e5297192fe9d512f247585
Victor LEDEZ <ledezvictor@gmail.com>
2022-06-22 16:26:29 +0200

    added test audio

--- src/music_features/config/main.py
+++ src/music_features/config/main.py
@@ -3,10 +3,11 @@ T_BEFORE_START = 10
 DURATION = 30
 
 # AUDIO_PATH = 'src/music_features/audio_samples/DJSnake_TurnDownForWhat.wav'
-# AUDIO_PATH = 'src/music_features/audio_samples/PharrellWilliams_Happy.wav'
+AUDIO_PATH = 'src/music_features/audio_samples/PharrellWilliams_Happy.wav'
 # AUDIO_PATH = 'src/music_features/audio_samples/BrunoMars_24kmagic.wav'
 # AUDIO_PATH = 'src/music_features/audio_samples/50Cent_CandyShop.wav'
+# AUDIO_PATH = 'src/music_features/audio_samples/SubcultureSage.wav'
+# AUDIO_PATH = 'src/music_features/audio_samples/50Cent_GetUp.wav'
 # AUDIO_PATH = 'src/music_features/audio_samples/Toto_Africa.wav'
 # AUDIO_PATH = 'src/music_features/audio_samples/DragonBall.wav'
-# AUDIO_PATH = 'src/music_features/audio_samples/50Cent_GetUp.wav'
-AUDIO_PATH = 'src/music_features/audio_samples/Jetta.wav'
+# AUDIO_PATH = 'src/music_features/audio_samples/Jetta.wav'
