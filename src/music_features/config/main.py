ed865209517a69478f2a1f531a72f6ab797c156b
Victor LEDEZ <ledezvictor@gmail.com>
2022-06-27 14:52:23 +0200

    added str

--- src/music_features/config/main.py
+++ src/music_features/config/main.py
@@ -6,8 +6,8 @@ DURATION = 30
 
 # AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
 #     "audio_samples/DJSnake_TurnDownForWhat.wav")
-# AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
-#     "audio_samples/PharrellWilliams_Happy.wav")
+AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
+    "audio_samples/PharrellWilliams_Happy.wav")
 # AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
 #     "audio_samples/BrunoMars_24kmagic.wav")
 # AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
@@ -18,10 +18,10 @@ DURATION = 30
 #     "audio_samples/SubcultureSage.wav")
 # AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
 #     "audio_samples/50Cent_GetUp.wav")
-# AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
-#     "audio_samples/Toto_Africa.wav")
 AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
-    "audio_samples/DragonBall.wav")
+    "audio_samples/Toto_Africa.wav")
+# AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
+#     "audio_samples/DragonBall.wav")
 # AUDIO_PATH = pathlib.Path(__file__).parent.parent.joinpath(
 #     "audio_samples/Jetta.wav")
 
--- src/test_audio.py
+++ src/test_audio.py
@@ -3,6 +3,8 @@ from music_features.config.main import (AUDIO_PATH, DURATION, SAMPLING_RATE,
 from music_features.main import music_features
 from music_features.beat_tracking import get_downbeats
 
+AUDIO_PATH = str(AUDIO_PATH)
+
 downbeats_frequency, downbeat_times = get_downbeats(AUDIO_PATH)
 
 all_segments, t_start_sequence, t_downbeat_max, t_end_sequence = music_features(
