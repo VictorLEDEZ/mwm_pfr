04656f8ff259b91d86af0ce39e67910c783b9a5e
pmecchia <mecchia.pierre@gmail.com>
2022-06-28 23:31:41 +0200

    uncomment code

--- src/main.py
+++ src/main.py
@@ -17,15 +17,15 @@ warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
 
-    # if len(sys.argv) != 5:
-    #     print("""
-    #     Four arguments are required:
-    #         - The first argument should be the path to the videos directory
-    #         - The second argument is the name of the clip
-    #         - The third argument is an integer for the summary duration time in seconds
-    #         - The fourth argument is an integer for the percentage of one shot summary
-    #         """)
-    #     sys.exit(0)
+    if len(sys.argv) != 5:
+        print("""
+        Four arguments are required:
+            - The first argument should be the path to the videos directory
+            - The second argument is the name of the clip
+            - The third argument is an integer for the summary duration time in seconds
+            - The fourth argument is an integer for the percentage of one shot summary
+            """)
+        sys.exit(0)
 
     videos_path = sys.argv[1]
     clip_filename = sys.argv[2]
@@ -37,39 +37,32 @@ if __name__ == '__main__':
     audio_sequence_path = pathlib.Path(__file__).parent.joinpath(
         'music_features/audio_sequence.wav')
 
-    # AUDIO_PATH = str(AUDIO_PATH)  # convert pathlib to string
+    AUDIO_PATH = str(AUDIO_PATH)  # convert pathlib to string
 
-    # videos_order = ordering_videos(videos_path)
+    videos_order = ordering_videos(videos_path)
 
-    # frames_list, summary_fps, summary_resolution = read_and_save_frames(videos_order)
+    frames_list, summary_fps, summary_resolution = read_and_save_frames(videos_order)
 
-    # downbeats_frequency, downbeat_times = get_downbeats(AUDIO_PATH)  # need to change frequency to time in seconds
+    downbeats_frequency, downbeat_times = get_downbeats(AUDIO_PATH)
 
-    # nb_shots = 10
-    # shots = define_shots(frames_list, summary_fps, nb_shots, shot_percentage, downbeats_frequency,show_viz=True)
+    nb_shots = 10
+    shots = define_shots(frames_list, summary_fps, nb_shots, shot_percentage, downbeats_frequency,show_viz=True)
 
-    # #summary_fps, summary_resolution = summary_param(videos_param)
+    dict_shots_order = score(frames_list, shots, sampling_rate=10)
 
-    # dict_shots_order = score(frames_list, shots, sampling_rate=10)
+    min_shot_nb = len(videos_order)
 
-    # min_shot_nb = len(videos_order)
+    summary_frames_index, time_before_drop, summary_duration = summary_frames_selection(
+        summary_duration, summary_fps, shot_percentage, dict_shots_order, min_shot_nb, downbeats_frequency)
 
-    # summary_frames_index, time_before_drop, summary_duration = summary_frames_selection(
-    #     summary_duration, summary_fps, shot_percentage, dict_shots_order, min_shot_nb, downbeats_frequency)
+    print('time before drop:', time_before_drop)
+    print("total duration summary", summary_duration)
 
-    # print('time before drop:', time_before_drop)
-    # print("total duration summary", summary_duration)
+    all_segments, t_start_sequence, t_downbeat_max, t_end_sequence = music_features(
+        AUDIO_PATH, summary_duration, SAMPLING_RATE, time_before_drop, downbeat_times, printing=False, plotting=False)
 
-    # all_segments, t_start_sequence, t_downbeat_max, t_end_sequence = music_features(
-    #     AUDIO_PATH, summary_duration, SAMPLING_RATE, time_before_drop, downbeat_times, printing=False, plotting=False)
-
-    # # ! remove offsets
-
-    # #summary_frames_index = summary_frames_index[int(
-    # #    summary_fps*offset_start): len(summary_frames_index)-int((summary_fps*offset_end))]
-
-    # create_summary(frames_list, summary_frames_index,
-    #                summary_path, summary_resolution, summary_fps)
+    create_summary(frames_list, summary_frames_index,
+                   summary_path, summary_resolution, summary_fps)
 
     create_clip(summary_video_path=summary_path,
                 audio_path=audio_sequence_path, clip_filename=clip_filename)
