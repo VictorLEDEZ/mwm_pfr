90171be7127e731e0e2e330dd74546580df2541a
pmecchia <mecchia.pierre@gmail.com>
2022-06-29 20:00:12 +0200

    add fadeout audio file

--- src/music_features/cut_music.py
+++ src/music_features/cut_music.py
@@ -1,6 +1,33 @@
 from pydub import AudioSegment
 import pathlib
 
+import numpy as np
+import librosa
+import soundfile
+
+
+def apply_fadeout(audio, sr, duration=3.0):
+    """
+    Function that creates a fade at the end of audio
+
+    Args:
+        audio (ndarray): librosa audio ndarray
+        sr (int): sample rate
+        duration (float): fade duration en seconds
+    """
+
+
+    length = int(duration * sr)
+    end = len(audio)
+    start = end - length
+
+    # linear fade
+    fade_curve = np.linspace(1.0, 0.0, length)
+
+    # apply the curve
+    audio[start:end] = audio[start:end] * fade_curve
+
+
 
 def cut_music(t1, t2, audio_path):
     """cuts the audio file on the sequence
@@ -11,11 +38,8 @@ def cut_music(t1, t2, audio_path):
         audio_path (string): path of the audio file
     """
 
-    t1 = t1 * 1000  # Works in milliseconds
-    t2 = t2 * 1000
+    new_audio, sr = librosa.load(audio_path)
+    new_audio = new_audio[int(t1*sr):int(t2*sr)]
+    apply_fadeout(new_audio, sr, duration=3.0)
 
-    newAudio = AudioSegment.from_wav(audio_path)
-    newAudio = newAudio[t1:t2]
-
-    newAudio.export(pathlib.Path(__file__).parent.joinpath(
-    "audio_sequence.wav"), format="wav")
+    soundfile.write(pathlib.Path(__file__).parent.joinpath("audio_sequence.wav"), new_audio, samplerate=sr)
